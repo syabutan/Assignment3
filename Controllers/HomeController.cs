d075a1c09aac48b1b48205bb1ed1ae4722ca9100
syabutan <71088052+syabutan@users.noreply.github.com>
2021-02-02 17:32:50 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieInfo info)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddMovieInfo(info);
+                return View("Confirmation", info);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult MovieCollection()
+        {            
+            return View(TempStorage.Infos);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public static class TempStorage
+    {
+        private static List<MovieInfo> infos = new List<MovieInfo>();
+
+        public static IEnumerable<MovieInfo> Infos => infos;
+
+        public static void AddMovieInfo(MovieInfo info)
+        {
+            infos.Add(info);
+        }
+    }
+}
